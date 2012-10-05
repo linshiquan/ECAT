88c6c54ccc6818f1fb71751ffc0a36ca0c11b72e
linshiquan <linshiquan@hotmail.com>
2012-10-05 13:38:19 +0800

    提交详情页

--- WebContent/app/view/ImageDetailView.js
+++ WebContent/app/view/ImageDetailView.js
@@ -79,7 +79,7 @@ Ext.define("ECAT.view.ImageDetailView", {
 	    //  me.timeout = Ext.defer(me.rotate, me.delay, me);
   	},
   	onActiveItemChange: function(c,v,ov){
-	    var me = this;
+		var me = this;
 	    
 	   // if (me.timeout){
 	   //   clearTimeout(me.timeout);
@@ -89,29 +89,37 @@ Ext.define("ECAT.view.ImageDetailView", {
 	    var active = c.getActiveIndex(),
 	        direction = (c.getItems().indexOf(v) > c.getItems().indexOf(ov)) ? 'forward' : 'backward',
 	        container = (direction=='forward') ? c.getAt(active-me.interval-1) : c.getAt(active+me.interval+1),
-	        store = this.config.store,
+			store = this.config.store, 
 			index = this.getComponent(active).index;
-	    
+	
 	    c.remove(container, false);
-	    
-	    if(direction=='forward'){
-	    	var next = index + 2;
-	    	if(next >= store.getCount()){
-	    		next = next - store.getCount();
-	    	}
-	    	var nextModel = store.getAt(next);
-	    	container.setSrc("resources/images/da/"+ nextModel.get('name') + ".png");
-	    	container.index = next;
-	    	 c.add(container);
-	    } else{
-	    	var last = index - 2;
-			if(last < 0){
-    			last = store.getCount() + last;
-    		}
-    		var lastModel = store.getAt(last);
-			container.setSrc("resources/images/da/"+ lastModel.get('name') + ".png");
-			container.index = last;
-	    	c.insert(0,container);
-	    }
-  }
+
+		if (direction == 'forward') {
+			this.setImageSrcForForward(container, store, index);
+			c.add(container);
+		} else {
+			this.setImageSrcForBackward(container, store, index);
+			c.insert(0, container);
+		}
+  },
+  setImageSrcForForward : function(container, store, index) {
+		var next = index + 2;
+		if (next >= store.getCount()) {
+			next = next - store.getCount();
+		}
+		var nextModel = store.getAt(next);
+		container.setSrc("resources/images/da/" + nextModel.get('name')
+				+ ".png");
+		container.index = next;
+	},
+	setImageSrcForBackward : function(container, store, index) {
+		var last = index - 2;
+		if (last < 0) {
+			last = store.getCount() + last;
+		}
+		var lastModel = store.getAt(last);
+		container.setSrc("resources/images/da/" + lastModel.get('name')
+				+ ".png");
+		container.index = last;
+	}
 });
